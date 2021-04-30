2f6918997e39c0bd9e5980c3d18ae08c8f921b35
easports2019 <56278766+easports2019@users.noreply.github.com>
2021-04-30 19:25:01 +0300

    300421-02
    Add+edit form TournamentItem

--- src/components/Panels/AdminPanel/Tournament/TournamentItem.jsx
+++ src/components/Panels/AdminPanel/Tournament/TournamentItem.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { RichCell, Avatar, FormLayout, FormItem, Input, InfoRow, Group, DatePicker, Textarea, File, CellButton, Button } from '@vkontakte/vkui'
 import { defaultPhotoPath } from '../../../../store/dataTypes/common'
+import { setTournamentWhenBegin, setTournamentWhenEnd, setTournamentName, setTournamentReglament, setTournamentDetails } from '../../../../store/tournamentsReducer'
 import { Icon24Camera, Icon28AddOutline } from '@vkontakte/icons';
+import { connect } from 'react-redux';
 
 
 
@@ -32,26 +34,29 @@ const TournamentItem = (props) => {
                     <InfoRow>Город (автоматически)</InfoRow>
                     <FormLayout>
                         <FormItem top="Название турнира" bottom="Имя турнира должно быть уникальным">
-                            <Input type="text" placeholder="Например, II чемпионат города Истра 2023 года на призы..." />
+                            <Input type="text" defaultValue={props.tournaments.selected.Name} onChange={e => props.setTournamentName(e.currentTarget.value)} placeholder="Например, II чемпионат города Истра 2023 года на призы..." />
                         </FormItem>
                         <FormItem top="Дата начала">
                             <DatePicker
                             min={{day: 1, month: 1, year: currentDate.getFullYear()-1}}
                             max={{day: 1, month: 1, year: currentDate.getFullYear()+1}}
-                            defaultValue={{day: currentDate.getDay(), month: currentDate.getMonth()+1, year: currentDate.getFullYear()}}
-                            onDateChange={(value) => {console.log(value)}}
+                            defaultValue={props.tournaments.selected.WhenBegin}
+                            onDateChange={value => props.setTournamentWhenBegin(value)}
                             />
                         </FormItem>
                         <FormItem top="Дата окончания">
                             <DatePicker
                             min={{day: 1, month: 1, year: currentDate.getFullYear()-1}}
                             max={{day: 1, month: 1, year: currentDate.getFullYear()+1}}
-                            defaultValue={{day: currentDate.getDay()+1, month: currentDate.getMonth()+1, year: currentDate.getFullYear()}}
-                            onDateChange={(value) => {console.log(value)}}
+                            defaultValue={props.tournaments.selected.WhenEnd}
+                            onDateChange={value => props.setTournamentWhenEnd(value)}
                             />
                         </FormItem>
                         <FormItem top="Описание турнира">
-                          <Textarea placeholder="Описание, регламент турнира" />
+                            <Textarea defaultValue={props.tournaments.selected.Details} onChange={e => props.setTournamentDetails(e.currentTarget.value)} placeholder="Описание турнира" />
+                        </FormItem>
+                        <FormItem top="Регламент турнира">
+                            <Textarea defaultValue={props.tournaments.selected.Reglament} placeholder="Регламент турнира" onChange={e => props.setTournamentReglament(e.currentTarget.value)} />
                         </FormItem>
                         <FormItem top="Загрузите ваше фото">
                             <File before={<Icon24Camera />} controlSize="m">
@@ -76,26 +81,29 @@ const TournamentItem = (props) => {
                     <InfoRow>Город (автоматически)</InfoRow>
                     <FormLayout>
                         <FormItem top="Название турнира" bottom="Имя турнира должно быть уникальным">
-                            <Input type="text" placeholder="Например, II чемпионат города Истра 2023 года на призы..." />
+                            <Input type="text" defaultValue={props.tournaments.selected.Name} onChange={e => props.setTournamentName(e.currentTarget.value)} placeholder="Например, II чемпионат города Истра 2023 года на призы..." />
                         </FormItem>
                         <FormItem top="Дата начала">
                             <DatePicker
                             min={{day: 1, month: 1, year: currentDate.getFullYear()-1}}
                             max={{day: 1, month: 1, year: currentDate.getFullYear()+1}}
-                            defaultValue={{day: currentDate.getDay(), month: currentDate.getMonth()+1, year: currentDate.getFullYear()}}
-                            onDateChange={(value) => {console.log(value)}}
+                            defaultValue={props.tournaments.selected.WhenBegin}
+                            onDateChange={value => props.setTournamentWhenBegin(value)}
                             />
                         </FormItem>
                         <FormItem top="Дата окончания">
                             <DatePicker
                             min={{day: 1, month: 1, year: currentDate.getFullYear()-1}}
                             max={{day: 1, month: 1, year: currentDate.getFullYear()+1}}
-                            defaultValue={{day: currentDate.getDay()+1, month: currentDate.getMonth()+1, year: currentDate.getFullYear()}}
-                            onDateChange={(value) => {console.log(value)}}
+                            defaultValue={props.tournaments.selected.WhenEnd}
+                            onDateChange={value => props.setTournamentWhenEnd(value)}
                             />
                         </FormItem>
                         <FormItem top="Описание турнира">
-                          <Textarea placeholder="Описание, регламент турнира" />
+                            <Textarea defaultValue={props.tournaments.selected.Details}  onChange={e => props.setTournamentDetails(e.currentTarget.value)} placeholder="Описание турнира" />
+                        </FormItem>
+                        <FormItem top="Регламент турнира">
+                            <Textarea defaultValue={props.tournaments.selected.Reglament} placeholder="Регламент турнира" onChange={e => props.setTournamentReglament(e.currentTarget.value)} />
                         </FormItem>
                         <FormItem top="Загрузите ваше фото">
                             <File before={<Icon24Camera />} controlSize="m">
@@ -114,4 +122,12 @@ const TournamentItem = (props) => {
 
 }
 
-export default TournamentItem
+const mapStateToProps = (state) => {
+    return {
+        tournaments: state.tournamentsEntity,
+    }
+}
+
+export default connect(mapStateToProps, {
+    setTournamentWhenBegin, setTournamentWhenEnd, setTournamentName, setTournamentReglament, setTournamentDetails, 
+}) (TournamentItem)
--- src/store/tournamentsReducer.js
+++ src/store/tournamentsReducer.js
@@ -9,12 +9,30 @@ let demoCityTournamentAdmins = cityTournamentAdmins;
 const TOURNAMENT_SET_ALL_TOURNAMENTS = "TOURNAMENT_SET_ALL_TOURNAMENTS";
 const TOURNAMENT_SET_MODE = "TOURNAMENT_SET_MODE";
 const TOURNAMENT_SET_ALL_CITYTOURNAMENTADMINS = "TOURNAMENT_SET_ALL_CITYTOURNAMENTADMINS";
+const TOURNAMENT_SET_WHEN_BEGIN = "TOURNAMENT_SET_WHEN_BEGIN";
+const TOURNAMENT_SET_WHEN_END = "TOURNAMENT_SET_WHEN_END";
+const TOURNAMENT_SET_NAME = "TOURNAMENT_SET_NAME";
+const TOURNAMENT_SET_DETAILS = "TOURNAMENT_SET_DETAILS";
+const TOURNAMENT_SET_REGLAMENT = "TOURNAMENT_SET_REGLAMENT";
 const TOURNAMENT_SET_TOURNAMENT_BY_ID = "TOURNAMENT_SET_TOURNAMENT_BY_ID";
 
-
+const currentDate = new Date();
 
 const initState = {
     tournaments: [], // все турниры
+    selected: {
+        Name: "",
+        Year: 0,
+        WhenBegin: {day: currentDate.getDay(), month: currentDate.getMonth()+1, year: currentDate.getFullYear()},
+        WhenEnd: {day: currentDate.getDay(), month: currentDate.getMonth()+1, year: currentDate.getFullYear()},
+        Details: "",
+        Reglament: "", 
+        Logo: "",
+        CityId: -1,
+        TournamentGroups: [], 
+        Admins: [],
+
+    }, // выбранный для просмотра/создания/редактирования турнир
     myTournaments: [], // те, что я создал
     cityTournamentAdmins: [], // админы текущего города
     mode: "view", // режим отображения турнира ("view" - просмотр, "add" - добавление, "edit" - редактирование)
@@ -41,6 +59,46 @@ let tournamentReducer = (state = initState, action) => {
                 cityTournamentAdmins: [...action.cityTournamentAdmins],
             };
         }
+        case TOURNAMENT_SET_WHEN_BEGIN: {
+            return {
+                ...state,
+                selected: {...state.selected, 
+                    WhenBegin: action.when,
+                },
+            };
+        }
+        case TOURNAMENT_SET_WHEN_END: {
+            return {
+                ...state,
+                selected: {...state.selected, 
+                    WhenEnd: action.when,
+                },
+            };
+        }
+        case TOURNAMENT_SET_NAME: {
+            return {
+                ...state,
+                selected: {...state.selected, 
+                    Name: action.value,
+                },
+            };
+        }
+        case TOURNAMENT_SET_REGLAMENT: {
+            return {
+                ...state,
+                selected: {...state.selected, 
+                    Reglament: action.value,
+                },
+            };
+        }
+        case TOURNAMENT_SET_DETAILS: {
+            return {
+                ...state,
+                selected: {...state.selected, 
+                    Details: action.value,
+                },
+            };
+        }
         default: {
             return state;
         }
@@ -54,12 +112,48 @@ export const setTournaments = (tournaments) => {
     }
 }
 
+export const setTournamentWhenBegin = (when) => {
+    return {
+        type: TOURNAMENT_SET_WHEN_BEGIN,
+        when
+    }
+}
+
+export const setTournamentWhenEnd = (when) => {
+    return {
+        type: TOURNAMENT_SET_WHEN_END,
+        when
+    }
+}
+
 export const setTournamentMode = (mode) => {
     return {
         type: TOURNAMENT_SET_MODE,
         mode
     }
 }
+
+export const setTournamentName = (value) => {
+    return {
+        type: TOURNAMENT_SET_NAME,
+        value
+    }
+}
+
+export const setTournamentDetails = (value) => {
+    return {
+        type: TOURNAMENT_SET_DETAILS,
+        value
+    }
+}
+
+export const setTournamentReglament = (value) => {
+    return {
+        type: TOURNAMENT_SET_REGLAMENT,
+        value
+    }
+}
+
 
 
 export const setCityTournamentAdmins = (cityTournamentAdmins) => {
